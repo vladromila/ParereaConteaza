03b9957036d7798c55e9990e6044bdbdcd13c690
Vlad Romila <romilavlad2001@gmail.com>
2018-12-04 17:45:34 +0200

    Updates

---
+++ precache-manifest.ec2699bdd84738bdf9418d8e74122a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2eab1bbc1c5aee78e62",
+    "url": "/ParereaConteaza/static/js/runtime~main.b2eab1bb.js"
+  },
+  {
+    "revision": "9108123da83d29a86698",
+    "url": "/ParereaConteaza/static/js/main.9108123d.chunk.js"
+  },
+  {
+    "revision": "93d79b646c68b1e1bb55",
+    "url": "/ParereaConteaza/static/js/1.93d79b64.chunk.js"
+  },
+  {
+    "revision": "9108123da83d29a86698",
+    "url": "/ParereaConteaza/static/css/main.7348487a.chunk.css"
+  },
+  {
+    "revision": "3a7377f0ad24b543f6981f3cf2c1510f",
+    "url": "/ParereaConteaza/index.html"
+  }
+];
